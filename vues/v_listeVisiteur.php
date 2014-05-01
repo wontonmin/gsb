b1c45053925ab3c2909d006edda4fc08bcf39a50
lesamgames <Samuel@LeSam>
2014-05-01 17:27:10 -0300

    Ajout de liste déroulante pour le choix de la fiche

--- vues/v_listeVisiteur.php
+++ vues/v_listeVisiteur.php
@@ -17,7 +17,7 @@
 
                 ?> 
                 <option 
-                    <?php 
+                    <?php
                         //selection le visiteur choisie
                         if (isset($_REQUEST['lstVisiteur'])) {
                             if ($_REQUEST['lstVisiteur'] == $idUtilisateur) {
@@ -53,10 +53,31 @@
                     }
                 ?>
                 
-                <label class="titre">Mois :</label> <input value='<?php echo $in_mois; ?>'class="zone" type="text" name="mois" size="12" />
+                <label class="titre">Mois :</label>
+                <select name="mois">
+                    <option value="01" <?php if ($in_mois == "01") echo "selected";?> >01</option>
+                    <option value="02" <?php if ($in_mois == "02") echo "selected";?> >02</option>
+                    <option value="03" <?php if ($in_mois == "03") echo "selected";?> >03</option>
+                    <option value="04" <?php if ($in_mois == "04") echo "selected";?> >04</option>
+                    <option value="05" <?php if ($in_mois == "05") echo "selected";?> >05</option>
+                    <option value="06" <?php if ($in_mois == "06") echo "selected";?> >06</option>
+                    <option value="07" <?php if ($in_mois == "07") echo "selected";?> >07</option>
+                    <option value="08" <?php if ($in_mois == "08") echo "selected";?> >08</option>
+                    <option value="09" <?php if ($in_mois == "09") echo "selected";?> >09</option>
+                    <option value="10" <?php if ($in_mois == "10") echo "selected";?> >10</option>
+                    <option value="11" <?php if ($in_mois == "11") echo "selected";?> >11</option>
+                    <option value="12" <?php if ($in_mois == "12") echo "selected";?> >12</option>    
+                </select>
                 </br>
                 </br>
-                <label class="titre">Année :</label> <input value='<?php echo $in_annee; ?>' class="zone" type="text" name="annee" size="12" />
+                <label class="titre">Année :</label>
+                <select name="annee">
+                    <option value="2010" <?php if ($in_annee == "2010") echo "selected";?> >2010</option>
+                    <option value="2011" <?php if ($in_annee == "2011") echo "selected";?> >2011</option>
+                    <option value="2012" <?php if ($in_annee == "2012") echo "selected";?> >2012</option>
+                    <option value="2013" <?php if ($in_annee == "2013") echo "selected";?> >2013</option>
+                    <option value="2014" <?php if ($in_annee == "2014") echo "selected";?> >2014</option>
+                </select>
                 </br>
                 </br>
                 <input id="ok" type="submit" value="Valider" size="20" />
